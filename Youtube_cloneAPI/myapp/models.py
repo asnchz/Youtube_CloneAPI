dbe55ce656677190649d757432b40ae1c58c4562
asnchz <asnchz27@yahoo.com>
2021-10-13 17:07:52 -0500

    Finished updating models.py

--- Youtube_cloneAPI/myapp/models.py
+++ Youtube_cloneAPI/myapp/models.py
@@ -2,5 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Comment(models.Model):
+    videoId = models.CharField(max_length=50)
     comment = models.CharField(max_length=150)
+    like = models.IntegerField(default=0)
+    dislike = models.IntegerField(default=0)
+
+class Reply(models.Model):
+    comment = models.ForeignKey(Comment, on_delete=models.CASCADE)
     reply = models.CharField(max_length=150)
